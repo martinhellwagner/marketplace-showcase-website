30bdc7823b0eed6f416821de2562d53321ffe6f2
Martin Hellwagner <martin@fredmansky.at>
2019-01-03 16:09:52 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- Template -->
+<template>
+  <div class="container">
+    <span class="description">
+      <b>Willkommen bei Movinga Umzüge.</b><br>
+      Biste dumm oder was?
+    </span>
+  </div>
+</template>
+
+<!-- Style -->
+<style lang="scss">
+  @import "./assets/style.scss";
+</style>
